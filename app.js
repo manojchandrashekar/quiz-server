9e7788e88c98514e54c98619aface3af2a03af03
Manoj Chandrashekar <manoj.chandrashekar@tarento.com>
2017-12-12 14:43:25 +0530

    Enabled CORS

--- app.js
+++ app.js
@@ -21,6 +21,12 @@ var ServerOptions = {
 };
 var server = https.createServer(ServerOptions, app);
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use(bodyParser.urlencoded({
     extended: true
 }));
